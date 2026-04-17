08751847d8371c5258d689f8ae52173821bdd0bb
EnlaiDo <enlaido05@outlook.com>
2026-04-17 10:51:03 -0500

    fix picture and center things

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,6 +11,7 @@ function Navbar() {
         <li><a href="#projects">Projects</a></li>
         <li><a href="#blog">Blog</a></li>
         <li><a href="#contact">Contact</a></li>
+        <li><a href="#skills">Skills</a></li>
       </ul>
     </nav>
   )
